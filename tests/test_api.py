2ad392af52b9d9387996cc7ebe8ce42915bf1c31
OussamaMadani <oussama@example.com>
2026-08-19 21:23:19 -0400

    test: demonstrate CI failure detection

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,7 +8,7 @@ def test_read_root():
         response = client.get("/")
 
     assert response.status_code == 200
-    assert response.json() == {"message": "API is up and running!"}
+    assert response.json() == {"message": "API is intentionally broken!"}
 
 
 def test_predict():
